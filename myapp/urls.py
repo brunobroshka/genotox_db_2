c34ca0cb523346a4c40e3564adc0e52676c7cab9
AdrianCabreraPhi <adriancabproven@gmail.com>
2025-03-25 16:00:14 +0100

    small changes

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 from .views import QueryAPIView
 urlpatterns = [
-    path('query/', QueryAPIView.as_view(), name='query_api'),
+    path('api/query/', QueryAPIView.as_view(), name='query_api'),
 ]
 
 
